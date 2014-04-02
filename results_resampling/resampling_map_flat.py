8eda0ea577115e29b8c6f6ddb6436c8650c10cd3
jdherman <jdherman8@gmail.com>
2014-04-02 09:55:21 -0400

    update scripts for job dependency

--- results_resampling/resampling_map_flat.py
+++ results_resampling/resampling_map_flat.py
@@ -57,7 +57,7 @@ for i in xrange(0, lat.size):
     idx = np.where((np.floor(a_lat) == np.floor(lat[i])) & (np.floor(a_lon) == np.floor(lon[i])))
     array[ilat,ilon] = max(ninety_revisit[idx] - qsurf[i], 0.001)
     
-array[array == 0] = np.NaN
+array[:] = np.NaN
 #m.contourf(x, y, array, np.arange(-1.0, 1.05, 0.1), cmap=cm.jet_r)
 ice = mpl.colors.ListedColormap(np.loadtxt('cmaps/ice.txt')/255)
 fire = mpl.colors.ListedColormap(np.loadtxt('cmaps/fire.txt')/255)
@@ -66,7 +66,7 @@ fire2 = mpl.colors.ListedColormap(np.power(np.loadtxt('cmaps/fire.txt'),1.5)/np.
 
 array_mask = np.ma.masked_where(np.isnan(array),array)
 
-m.pcolormesh(x,y,array_mask,vmin=0.0,vmax=7.0, cmap=fire2, rasterized=True)
+m.pcolormesh(x,y,array_mask,vmin=0.0,vmax=7.0, cmap=fire2, rasterized=True, edgecolor='0.6', linewidth=0)
 cbar = m.colorbar()
 cbar.solids.set_edgecolor("face")
 cbar.set_ticks([0,1,2,3,4,5,6,7])
--- sh/submit_all_jobs.sh
+++ sh/submit_all_jobs.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 FILENUMS=$(seq 0 199)
+MAX_SIMULT=7 # max number of simultaneous jobs
 
 for FILENUM in ${FILENUMS}
 do
   NAME=hcube_${FILENUM}_9p_50
   echo "Submitting: ${NAME}"
+  JOBCOUNT=$(($FILENUM % $MAX_SIMULT))
 
-  qsub -N ${NAME} -v NUM=${FILENUM} Run_Hcube.sh
+  if [ $JOBCOUNT -eq 0]
+  then
+    DEPEND=`qsub -N ${NAME} -v NUM=${FILENUM} Run_Hcube.sh`
+  else
+    qsub -N ${NAME} -W depend=afterok:${DEPEND} -v NUM=${FILENUM} Run_Hcube.sh
+  fi
   
   sleep 0.5
 
