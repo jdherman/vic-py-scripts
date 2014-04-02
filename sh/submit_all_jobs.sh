2f7e043c14e2553a4db0233786c24ef84c561d8a
jdherman <jdherman8@gmail.com>
2014-04-02 10:39:41 -0400

    update submission script

--- sh/submit_all_jobs.sh
+++ sh/submit_all_jobs.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
+# This submission will submit 200 jobs in "globs", 
+# Where one glob will not begin until the previous one finishes without errors
+
 FILENUMS=$(seq 0 199)
 MAX_SIMULT=7 # max number of simultaneous jobs
+GLOBNUM=0
+GLOBNUM_PREV=0
+DEPEND=""
+DEPEND_PREV=""
 
 for FILENUM in ${FILENUMS}
 do
   NAME=hcube_${FILENUM}_9p_50
   echo "Submitting: ${NAME}"
-  JOBCOUNT=$(($FILENUM % $MAX_SIMULT))
+  GLOBNUM=$(($FILENUM / $MAX_SIMULT))
 
-  if [ $JOBCOUNT -eq 0]
+   # the first MAX_SIMULT jobs have no dependency
+   # submit the jobs and append to string
+  if [ $GLOBNUM -eq 0 ]
   then
-    DEPEND=`qsub -N ${NAME} -v NUM=${FILENUM} Run_Hcube.sh`
+    DEPEND=$DEPEND:`qsub -N ${NAME} -v NUM=${FILENUM} Run_Hcube.sh`
+
+  # Else if the counter ticks ahead to the next "glob" ...
+  # Grab the existing job dependencies and start building the next string
+  elif [ $GLOBNUM -ne $GLOBNUM_PREV ]
+  then
+    DEPEND_PREV=$DEPEND
+    DEPEND=`qsub -N ${NAME} -W depend=afterok${DEPEND_PREV} -v NUM=${FILENUM} Run_Hcube.sh`
+    GLOBNUM_PREV=$GLOBNUM
+
+  # If we're in the same glob as the previous iteration, use the same dependency string
   else
-    qsub -N ${NAME} -W depend=afterok:${DEPEND} -v NUM=${FILENUM} Run_Hcube.sh
+    DEPEND=$DEPEND:`qsub -N ${NAME} -W depend=afterok${DEPEND_PREV} -v NUM=${FILENUM} Run_Hcube.sh`
   fi
   
   sleep 0.5
+  echo $DEPEND
 
 done
