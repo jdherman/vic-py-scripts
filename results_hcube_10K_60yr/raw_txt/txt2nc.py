65d98dfeeea319909f28b099f2a3d25860fdd91f
jdherman <jdherman8@gmail.com>
2014-04-29 10:37:21 -0400

    finished conversion to netcdf

--- results_hcube_10K_60yr/raw_txt/txt2nc.py
+++ results_hcube_10K_60yr/raw_txt/txt2nc.py
@@ -23,24 +23,15 @@ ensemble = root_grp.createDimension('ensemble', 10000)
 latitudes = root_grp.createVariable('latitude', 'f4', ('lat',))
 longitudes = root_grp.createVariable('longitude', 'f4', ('lon',))
 vic_runoff = root_grp.createVariable('vic_runoff', 'f4', ('lat', 'lon', 'ensemble', 'month',), fill_value=-9999.0)
-obs_runoff = root_grp.createVariable('obs_runoff', 'f4', ('lat', 'lon', 'month'))
-annual_error = root_grp.createVariable('annual_error', 'f4', ('lat', 'lon', 'ensemble',))
-monthly_error = root_grp.createVariable('monthly_error', 'f4', ('lat', 'lon', 'ensemble', 'month',))
+obs_runoff = root_grp.createVariable('obs_runoff', 'f4', ('lat', 'lon', 'month'), fill_value=-9999.0)
 
 vic_runoff.units = 'mm/month'
 obs_runoff.units = 'mm/month'
-annual_error.units = '%'
-monthly_error.units = '%'
 
 # set the variables we know first
 latitudes = np.arange(-90.5, 89.5, 1.0)
 longitudes = np.arange(0.5, 360.5, 1.0)
-
-# keep values in memory until ready to write a big chunk to NC file
-temp_vic = np.zeros((180, 360, len(ensemble), 12), float)
-temp_obs = np.zeros((180, 360, 12), float)
-temp_annual_err = np.zeros((180, 360, len(ensemble)), float)
-temp_monthly_err = np.zeros((180, 360, len(ensemble), 12), float)
+cellsdone = 0
 
 for lati, lat in enumerate(latitudes):
   for loni, lon in enumerate(longitudes):
@@ -54,7 +45,10 @@ for lati, lat in enumerate(latitudes):
       current_obs = np.squeeze(current_obs)
 
       if(current_obs.size > 0):
-        temp_obs[lati,loni,:] = current_obs
+        obs_runoff[lati,loni,:] = current_obs
+
+      # keep values in memory until ready to write a big chunk to NC file
+      tempstore = np.zeros((len(ensemble), 12), float)
 
       for filenum in xrange(0,200):
 
@@ -67,19 +61,19 @@ for lati, lat in enumerate(latitudes):
               pass
 
           # write the VIC output data
-          ensemble_start = filenum*50
-          temp_vic[lati,loni, filenum*50:(filenum+1)*50 ,:] = output # ensembles x months
+          startix = filenum*50
+          tempstore[startix:(startix+output.shape[0]),:] = output # ensembles x months
+      
+      vic_runoff[lati,loni,:,:] = tempstore # write all ensembles to netcdf
+      cellsdone = cellsdone + 1
+      print cellsdone
 
-# also get error values -- ignore for right now
+# also get error values
 # for j in xrange(0, len(ensemble)):
-#   temp_annual_err[:,:,j] = 100*np.fabs(np.sum(temp_obs, axis=2) - np.sum(vic_runoff[j,:,:,:], axis=0))/np.sum(obs_runoff, axis=0)
+#   annual_error[j,:,:] = 100*np.fabs(np.sum(obs_runoff, axis=0) - np.sum(vic_runoff[j,:,:,:], axis=0))/np.sum(obs_runoff, axis=0)
 
 #   for m in xrange(0, 12):
-#     temp_monthly_err[:,:,j,m] = 100*np.fabs(obs_runoff[m,:,:] - vic_runoff[j,m,:,:])/obs_runoff[m,:,:]
+#     monthly_error[j,m,:,:] = 100*np.fabs(obs_runoff[m,:,:] - vic_runoff[j,m,:,:])/obs_runoff[m,:,:]
 
-vic_runoff = temp_vic
-obs_runoff = temp_obs
-# annual_error = temp_annual_err
-# monthly_error = temp_monthly_err
 
 root_grp.close()
